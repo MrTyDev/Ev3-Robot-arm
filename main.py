609d833ede44b0cd588529967562cf8fc1811a42
Hadi Abou Zahra <guntrix3rhd@gmail.com>
2023-04-06 15:27:49 +0200

    whike

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def pickup(position):
 
     # Lower the arm.
     elbow_motor = arm_motor
-    elbow_motor.run_until_stalled(-1000, then=Stop.HOLD, duty_limit=70)
+    elbow_motor.run_until_stalled(-1000, then=Stop.HOLD, duty_limit=30)
 
     # Close the gripper to grab the wheel stack.
     gripper_motor = claw_motor
@@ -39,6 +39,9 @@ def pickup(position):
 
     # Raise the arm to lift the wheel stack.
     elbow_motor.run_target(500, 0) 
+
+    while True:
+        gripper_motor.run_until_stalled(600, then=Stop.HOLD, duty_limit=100)
 
 def drop():
     claw_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=50)
