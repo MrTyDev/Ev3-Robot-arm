2074e55541420e279dd91f9d81bd7504bf9e14fb
Hadi Abou Zahra <guntrix3rhd@gmail.com>
2023-05-10 10:34:13 +0200

    sds

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def pickup(pickup_position,elbow_taget):
         gripper_motor.run_angle(100,-100)
     #print(base_motor.angle())
     wait(500)
-    if color_recognition() == Color.RED or color_recognition() == Color.BLUE or color_recognition() == Color.YELLOW or color_recognition() == Color.GREEN or color_recognition() == Color.WHITE:
+    if str(color_recognition()) in color_positions:
         color = color_recognition()
         ev3.speaker.say((str(color).lower()).replace("color.", ""))
         
